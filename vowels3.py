3dc24fef66294f61eea05907ec5a8d6e52ae7a84
jucgoods <jucgoods@jucgoodss-imac.int.uncommongoods.com>
2019-05-10 15:53:57 -0400

    vowels lesson for the kids

---
+++ vowels3.py
@@ -0,0 +1,10 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("proviide a word to search for vowels: ")
+found = []
+for le in word:
+    if le in vowels:
+        if le not in found:
+            found.append(le)
+for vowel in found:
+    print(vowel)
+ 
